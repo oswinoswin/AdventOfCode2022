3726769fccf2cdd170d86162c68bcac202b94247
oswinoswin <laura.uchwat@gmail.com>
2022-12-13 14:24:15 +0100

    06

--- 06/06.py
+++ 06/06.py
@@ -1,13 +1,25 @@
 def first(filename):
     with open(filename, "r") as f:
-        for line in f:
-            print(line)
+        line = f.readline()
+        for i in range(4, len(line)):
+            sequence = line[i - 4: i]
+            unique_chars_number = len(set(sequence))
+            # print(sequence, i)
+            if unique_chars_number == 4:
+                return i
 def second(filename):
     with open(filename, "r") as f:
-        for line in f:
-            print(line)
+        line = f.readline()
+        # print(line, len(line))
+        for i in range(14, len(line)):
+            sequence = line[i - 14: i]
+            unique_chars_number = len(set(sequence))
+            # print(sequence, i, unique_chars_number)
+            if unique_chars_number == 14:
+                return i
+
 if __name__ == "__main__":
-    print(first("example.txt"))
-    print(first("input.txt"))
+    # print(first("example.txt"))
+    # print(first("input.txt"))
     print(second("example.txt"))
     print(second("input.txt"))
